2e7e2c242e7d8ce2cf7872f97ab2d5b0329d11d8
el3c_tron <aniketkumar28032002@gmail.com>
2024-05-14 13:09:31 +0530

    Tailwind and props

---
+++ tailwind_css/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Heading from './components/Heading'
+
+function App() {
+
+  return (
+    <>
+      <Heading content="Heading1" />
+      <Heading content="Heading2"/>
+      <Heading content="Heading3"/>
+    </>
+  )
+}
+
+export default App
