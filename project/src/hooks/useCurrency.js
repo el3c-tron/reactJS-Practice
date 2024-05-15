ff7b858d6c83eb3e8de3893ac598badc47e9fc01
el3c_tron <aniketkumar28032002@gmail.com>
2024-05-15 20:45:44 +0530

    Currency Converter

--- project/src/hooks/useCurrency.js
+++ project/src/hooks/useCurrency.js
@@ -4,11 +4,11 @@ function useCurrency(currency) {
     
 
     const [data, setData] = useState({})
-    useEffect(() => {
-        fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@2024-03-06/v1/currencies/${currency}.json`)
+
+    fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@2024-03-06/v1/currencies/${currency}.json`)
         .then((res) => res.json())
         .then((res) => setData(res[currency]))
-    }, [currency])
+    
     return data
 
 }
